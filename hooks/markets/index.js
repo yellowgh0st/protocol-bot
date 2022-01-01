99e30d7b516696be9a739000bcf31cd67eb9c455
yellowgh0st <ZjAYxgG2kHyZbpuv@protonmail.com>
2022-01-01 13:59:15 +0100

    Advance mexc market

--- hooks/markets/index.js
+++ hooks/markets/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const uniswapV2Price = require('../../skills/uniswapV2Price')
 const defaults = require('../../core/defaults')
 const mexcTicker = require('../../skills/mexcTicker')
@@ -30,7 +29,18 @@ module.exports = {
 					},
 				}
 
-				const mexcMarket = await mexcTicker('VADER_USDT')
+				const mexcMarket = {
+					'eth': {
+						'usdt': await mexcTicker('ETH_USDT'),
+					},
+					'vader': {
+						'usdt': await mexcTicker('VADER_USDT'),
+					},
+					'dai': {
+						'usdt': await mexcTicker('DAI_USDT'),
+					}
+				}
+
 				const poloniexMarket = {
 					'eth': {
 						'usdt': await poloniexTicker('USDT_ETH'),
@@ -45,9 +55,11 @@ module.exports = {
 
 				switch (exchange) {
 					case 'uniswap': announce = `<:uniswap:718587420274196553> Uniswap V2 **$VADER** price is at *USDC* **${uniswapV2Market.vader.usdc}**, *DAI* **${uniswapV2Market.vader.dai}**, *Ξ* **${uniswapV2Market.vader.weth}**`; break
-					case 'mexc': announce = `<:mxc:917129131244789781> MEXC Global **$VADER** price is at *USDT* **${await prettifyNumber(mexcMarket?.data?.[0]?.last, 0, 5)}**`; break
-					case 'poloniex': announce = `<:poloniex:925869327608070186> Poloniex **$VADER** price is at *USDT* **${await prettifyNumber(poloniexMarket.vader.usdt['last'], 0, 5)}**, *DAI* **${await prettifyNumber(Number(Number(poloniexMarket.vader.usdt['last']) * Number(poloniexMarket.usdt.dai['last'])), 0, 5)}**, *Ξ* **${await prettifyNumber(Number(Number(poloniexMarket.vader.usdt['last']) / Number(poloniexMarket.eth.usdt['last'])), 0, 10)}**`; break
-					default: announce = `<:uniswap:718587420274196553> Uniswap V2 **$VADER** price is at *USDC* **${uniswapV2Market.vader.usdc}**, *DAI* **${uniswapV2Market.vader.dai}**, *Ξ* **${uniswapV2Market.vader.weth}**\n<:mxc:917129131244789781> MEXC Global **$VADER** price is at *USDT* **${await prettifyNumber(mexcMarket?.data?.[0]?.last, 0, 5)}**\n<:poloniex:925869327608070186> Poloniex **$VADER** price is at *USDT* **${await prettifyNumber(poloniexMarket.vader.usdt['last'], 0, 5)}**, *DAI* **${await prettifyNumber(Number(Number(poloniexMarket.vader.usdt['last']) * Number(poloniexMarket.usdt.dai['last'])), 0, 5)}**, *Ξ* **${await prettifyNumber(Number(Number(poloniexMarket.vader.usdt['last']) / Number(poloniexMarket.eth.usdt['last'])), 0, 10)}**`
+					case 'mexc': announce = `<:mxc:917129131244789781> MEXC Global **$VADER** price is at *USDT* **${await prettifyNumber(mexcMarket?.vader.usdt.data?.[0]?.last, 0, 5)}**, *DAI* **${await prettifyNumber(Number(Number(mexcMarket?.vader.usdt.data?.[0]?.last) * Number(mexcMarket?.dai.usdt.data?.[0]?.last)), 0, 5)}**, *Ξ* **${await prettifyNumber((Number(mexcMarket?.vader.usdt.data?.[0]?.last) / Number(mexcMarket?.eth.usdt.data?.[0]?.last)), 0, 10)}**`; break
+					case 'poloniex': announce = `<:poloniex:925869327608070186> Poloniex **$VADER** price is at *USDT* **${await prettifyNumber(poloniexMarket.vader.usdt['last'], 0, 5)}**, *DAI* **${await prettifyNumber(Number(Number(poloniexMarket.vader.usdt['last']) * Number(poloniexMarket.usdt.dai['last'])), 0, 5)}**, *Ξ* **${await prettifyNumber((Number(poloniexMarket.vader.usdt['last']) / Number(poloniexMarket.eth.usdt['last'])), 0, 10)}**`; break
+					default: announce = `<:uniswap:718587420274196553> Uniswap V2 **$VADER** price is at *USDC* **${uniswapV2Market.vader.usdc}**, *DAI* **${uniswapV2Market.vader.dai}**, *Ξ* **${uniswapV2Market.vader.weth}**
+<:mxc:917129131244789781> MEXC Global **$VADER** price is at *USDT* **${await prettifyNumber(mexcMarket?.vader.usdt.data?.[0]?.last, 0, 5)}**, *DAI* **${await prettifyNumber(Number(Number(mexcMarket?.vader.usdt.data?.[0]?.last) * Number(mexcMarket?.dai.usdt.data?.[0]?.last)), 0, 5)}**, *Ξ* **${await prettifyNumber((Number(mexcMarket?.vader.usdt.data?.[0]?.last) / Number(mexcMarket?.eth.usdt.data?.[0]?.last)), 0, 10)}**
+<:poloniex:925869327608070186> Poloniex **$VADER** price is at *USDT* **${await prettifyNumber(poloniexMarket.vader.usdt['last'], 0, 5)}**, *DAI* **${await prettifyNumber((Number(poloniexMarket.vader.usdt['last']) * Number(poloniexMarket.usdt.dai['last'])), 0, 5)}**, *Ξ* **${await prettifyNumber((Number(poloniexMarket.vader.usdt['last']) / Number(poloniexMarket.eth.usdt['last'])), 0, 10)}**`
 				}
 		
 				if(announce) {
